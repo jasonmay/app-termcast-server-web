84378866b4230c445aaf1110f4fce9c43910e32b
Jason May <jason.a.may@gmail.com>
2010-10-31 23:15:34 -0400

    plackup obsoletes these scripts

--- bin/termcast-server-web.pl
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env perl
-use strict;
-use warnings;
-use App::Termcast::Server::Web;
-
-my $web = App::Termcast::Server::Web->new;
-
-$web->run;
--- tsw.sh
+++
@@ -1,3 +0,0 @@
-export PERL5LIB=lib:server/lib:../html-fromansi/lib:../termcast-server-web/lib:"$PERL5LIB"
-
-perl bin/termcast-server-web.pl
