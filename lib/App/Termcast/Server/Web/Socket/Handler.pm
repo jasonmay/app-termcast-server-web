b0f8b64aa699aefd74416f5b3fae36ffb66dc2ac
Jason May <jason.a.may@gmail.com>
2013-05-25 20:10:42 -0400

    tell server to prepopulate with buffer when ready

--- lib/App/Termcast/Server/Web/Socket/Handler.pm
+++ lib/App/Termcast/Server/Web/Socket/Handler.pm
@@ -2,6 +2,7 @@ package App::Termcast::Server::Web::Socket::Handler;
 use Moose;
 
 use App::Termcast::Server::Web::Socket;
+use JSON;
 
 has connections => (
     is       => 'ro',
@@ -19,7 +20,16 @@ sub run {
            my $t_socket = $self->connections->find_socket($socket);
            $t_socket->stream($stream) if $t_socket;
            $socket->emit('ready');
+
        });
+       $socket->on(ready => sub {
+           my ($socket) = @_;
+           my $t_socket = $self->connections->find_socket($socket);
+           my $stream = $t_socket->stream;
+           my $stream_data = $self->connections->get_stream($stream);
+           $t_socket->send_to_browser($stream_data->buffer);
+       });
+
        my $t_socket = App::Termcast::Server::Web::Socket->new(handle => $socket);
        $self->connections->socket_handles->insert($t_socket);
     };
